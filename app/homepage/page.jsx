09697ee3a9d3b06c09cea25da425e42ca471cfa7
Tran Nguyen Anh Minh <anhminhnts2004@gmail.com>
2023-08-21 16:06:13 +0700

    adjust the homepage based on the comments

--- app/homepage/page.jsx
+++ app/homepage/page.jsx
@@ -5,13 +5,13 @@ import "../css/utility.css"
 export default function HomePage() {
     return (
         // Name of the application
-        <div className="">
-            <div className="text-center flex flex-col items-center my-4 py-4">
-                <h1 className="w-full md:w-1/2 lg:w-1/3 text-blue-600 font-inter text-center">{metadata.description}</h1>
-                <h3 className="font-inter">{metadata.label}</h3>
+        <div className="flex flex-col max-h-screen items-center">
+            <div className="text-center flex flex-col items-center">
+                <h1 className="w-full lg:w-2/3 text-blue-600 h1 text-center m-0 py-2">{metadata.description}</h1>
+                <h3 className="text-center h3 m-0 py-2">{metadata.label}</h3>
             </div>
             {/* Input link and Link button*/}
-            <div className="flex justify-center py-4 relative">
+            <div className="flex justify-center py-3 relative w-full lg:w-3/4">
                 <span className="absolute inset-y-0 flex items-center left-4"><img src="./../link.svg" alt="link" /></span>
                 <input type="link" className="rounded-xl px-8 py-2 mx-2 text-lg shadow-around w-full border-transparent" placeholder="Connect your Excel link here ..." />
                 <span className="absolute inset-y-0 flex items-center right-4">
@@ -20,7 +20,7 @@ export default function HomePage() {
             </div>
             {/*Div contains the images of table and dashboard*/}
             <div className="flex justify-center">
-                <img src="./../homepage.svg" alt="Home page" className="w-screen relative -z-10 -mt-28 md:-mt-48 lg:-mt-72" />
+                <img src="./../homepage.svg" alt="Home page" className="w-full relative -z-10 -mt-24 md:-mt-32 lg:-mt-52" />
             </div>
         </div>
     )
