c5243e9d097bf5b2d7b41580e7ceb66927ad2348
Cullen179 <itslamemail@gmail.com>
2023-09-03 23:40:15 +0700

    Remove setup page

---
+++ app/(steup)/excel/page.jsx
@@ -0,0 +1,36 @@
+// import './../css/utility.css';
+// import './../css/globals.css';
+// import StepsBar from '../../components/StepsBar';
+
+// export default function StepOnePage() {
+//     return (
+//         <div className='flex flex-col justify-around items-center h-[calc(100vh-42px)]'>
+//             {/* Title and notification */}
+//             <div className="text-center">
+//                 <h1 className="font-jomhuria text-blue-600 h1 m-0">Checker</h1>
+//                 <h3 className="m-0 h3 font-inter">Your Check-in setup is <span className="text-blue-600 h3 font-inter">2</span> steps away!</h3>
+//             </div>
+//             {/* Dashboard contains an input element to enter Excel link and Link button */}
+//             <div className="border border-solid border-gray-400 rounded-2xl shadow-md shadow-gray-300 flex flex-col items-center py-14 px-5 w-full lg:w-3/4">
+//             {/* Display Steps imported from antd */}
+//                 <div className='w-3/5 my-4'>
+//                     <StepsBar/>
+//                 </div>
+//             {/* Contains a textfield and a "Ready" button to initiate the Link Excel phase */}
+//                 <div className="flex flex-col w-3/5 my-4">
+//                     <h2 className='text-left text-gray-500 p-2 m-0 font-inter'>Let's get started</h2>
+//                     <input type="link" className="text-lg border-transparent rounded-lg p-2 my-2 font-inter" placeholder="Your excel link..."/>
+//                     <button type="submit" className=" bg-blue-600 text-white rounded-lg border-transparent text-lg cursor-pointer p-2 my-2 font-inter font-bold">Ready</button>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+export default function Excel() {
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ utils/QuestionCard.jsx
@@ -0,0 +1,7 @@
+export default function QuestionCard({children}) {
+    return (
+        <div className="w-full border rounded-lg border-gray-300 shadow p-8">
+            {children}
+        </div>
+    )
+}
