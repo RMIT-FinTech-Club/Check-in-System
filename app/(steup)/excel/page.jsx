ace084a36f64bca6fbb02b8e87f9ff6d33a2e758
Cullen179 <itslamemail@gmail.com>
2023-09-04 10:49:09 +0700

    Update page.jsx

--- app/(steup)/excel/page.jsx
+++ app/(steup)/excel/page.jsx
@@ -7,7 +7,7 @@ export default function StepOnePage() {
             {/* Dashboard contains an input element to enter Excel link and Link button */}
             {/* Contains a textfield and a "Ready" button to initiate the Link Excel phase */}
                 <div>
-                    <h2 className='h3 text-black-200 mb-3'>Let's get started</h2>
+                    <h2 className='h3 text-black-200 mb-5'>Let's get started</h2>
                     <form action="">
                         <Input addonBefore={<LinkOutlined />} size="large" allowClear placeholder="Insert your Excel link..."/>
                         <Button size="large" type="primary" style={{width:'100%'}} className="mt-4">
