8911081d7b543a389752fc08ca30667d93ff6df3
Rainb0wBanana <s3977773@rmit.edu.vn>
2023-09-04 16:37:04 +0700

    Create ObjectDetection.jsx

---
+++ components/ObjectDetection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const WebcamFeed = () => {
+    return (
+        <>
+            <img
+                src={"api/id-detection/video_feed"}
+                // width="640"
+                // height="480"
+                alt="Camera Feed"
+            />
+        </>
+    )
+}
