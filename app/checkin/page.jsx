8b951df0604c7fc8acc03781b2d3d85db7cc6446
NguyenNguyen205 <92852929+NguyenNguyen205@users.noreply.github.com>
2023-09-06 10:52:25 +0700

    Add pop up camera checkin modal to page

--- app/checkin/page.jsx
+++ app/checkin/page.jsx
@@ -1,31 +1,28 @@
 "use client";
 
-import { CameraOutlined } from "@ant-design/icons";
 import { Button, Card, Modal } from "antd";
 import { useState } from "react";
 import ManualForm from "@/components/ManualForm";
 import StepBar from "@/components/StepBar";
+import CameraCheckin from "@/components/CameraCheckin";
 import Question from "@/utils/Question";
+import { useRouter } from "next/navigation";
 
 /**
  * A react component representing the checkin page
  */
 export default function Checkin() {
+  const router = useRouter();
+
+  // If there is no data, return to step 1
+  if (localStorage.getItem("questions") == null) {
+    return router.replace("/homepage");
+  }
+
   // Loading the questions information from the local storage
   const questions = JSON.parse(localStorage.getItem("questions")).map(
     (val) => new Question(val)
   );
-
-  // Handling modal opening and closing
-  const [open, setOpen] = useState(false);
-
-  const showModal = () => {
-    setOpen(true);
-  };
-
-  const handleCancel = () => {
-    setOpen(false);
-  };
 
   return (
     <div className="content">
@@ -44,43 +41,19 @@ export default function Checkin() {
       <Card className="shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
         <StepBar index={3}></StepBar>
 
-        {/* Modal for manual input form */}
-        <Modal
-          open={open}
-          // onOk={handleOk}
-          // confirmLoading={confirmLoading}
-          onCancel={handleCancel}
-          width={1000}
-          okButtonProps={{ style: { display: "none" } }}
-          cancelButtonProps={{ style: { display: "none" } }}
-        >
-          <h1 className="h2 text-center"> Manual Input Form </h1>
-          {/* Manual Input form component */}
-          <ManualForm questions={questions}></ManualForm>
-        </Modal>
-
         {/* Camera Button for opening camera and future checkin functions */}
         <div className="flex flex-col px-40">
           <p className="h3 text-center">
             {" "}
             Click on the camera to start recording{" "}
           </p>
-          <div className="flex justify-center">
-            <Button
-              className=" h-40 w-40 border-none flex justify-center items-center "
-              onClick={() => {}}
-            >
-              <CameraOutlined style={{ fontSize: "1000%" }} />
-            </Button>
-          </div>
-
-          {/* Manual Input Button */}
-          <Button ghost className=" mt-4 " type="primary" onClick={showModal}>
-            <span className="text-black-100"> Manual Input </span>
-          </Button>
+          {/* Camera Popup modal */}
+          <CameraCheckin questions={questions}></CameraCheckin>
+          {/* Manual Form Pop up modal */}
+          <ManualForm questions={questions}></ManualForm>
         </div>
         <div className="flex justify-end mt-4">
-          <Button className="mx-4 font-bold text-black-200 hidden p-4">
+          <Button className="mx-4 font-bold text-black-200 hidden p-4 w-full">
             Previous
           </Button>
         </div>
