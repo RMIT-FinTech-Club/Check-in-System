dd9b891eaf061d4198cf192678ae10c0b9c5ee38
NguyenNguyen205 <92852929+NguyenNguyen205@users.noreply.github.com>
2023-09-06 10:51:43 +0700

    Add Button component to the ManualForm component

--- components/ManualForm.jsx
+++ components/ManualForm.jsx
@@ -1,6 +1,16 @@
-import { Form, Button, Input, DatePicker, Radio, message, Space } from "antd";
+import {
+  Form,
+  Button,
+  Input,
+  DatePicker,
+  Radio,
+  message,
+  Space,
+  Modal,
+} from "antd";
 import Question from "@/utils/Question";
 import { validatesID, validatesName } from "@/utils/formValidator";
+import { useState } from "react";
 
 /**
  * A component for user to manually fill out form for the checkin process
@@ -15,7 +25,7 @@ export default function ManualForm({ questions }) {
 
     // Removing id from form input name and return (need updates)
     for (let [key, value] of Object.entries(values)) {
-      console.log(typeof value);
+      // console.log(typeof value);
       let mid = key.slice(0, key.length - 2);
       if (!(mid in result)) result[mid] = [];
       if (typeof value == "object") value = value.format("YYYY-MM-DD HH:mm:ss");
@@ -39,6 +49,17 @@ export default function ManualForm({ questions }) {
     });
   };
 
+  // Handling modal opening and closing
+  const [open, setOpen] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
   // Mapping each question type into different input field
   const typeMapping = {
     Text: <Input placeholder="Enter your text"></Input>,
@@ -49,89 +70,106 @@ export default function ManualForm({ questions }) {
 
   return (
     <>
-      {/* Pop up message when a form is submit */}
-      {contextHolder}
-
-      <Form
-        layout="vertical"
-        form={form}
-        name="manual input"
-        labelCol={{
-          span: 22,
-          offset: 1,
-        }}
-        wrapperCol={{
-          offset: 1,
-          span: 22,
-        }}
-        style={{
-          maxWidth: "100%",
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
+      {/* Modal for manual input form */}
+      <Modal
+        open={open}
+        // onOk={handleOk}
+        onCancel={handleCancel}
+        width={1000}
+        okButtonProps={{ style: { display: "none" } }}
+        cancelButtonProps={{ style: { display: "none" } }}
       >
-        {/* Mapping each question into an input field using the question attributes */}
-        {questions.map((question) => {
-          let title = question.title.slice();
-          let choices = question.choice;
-          return (
-            <Form.Item
-              key={question.id}
-              label={title.charAt(0).toUpperCase() + title.slice(1)}
-              name={`${question.title}-${question.id}`} // Combining name with object id to create unique name for form input
-              rules={[
-                {
-                  required: question.required,
-                  messsage: `Please input your ${title}`,
+        <h1 className="h2 text-center"> Manual Input Form </h1>
+        {/* Manual Input form component */}
 
-                  type: question.type == "Date" ? "object" : "string",
-                },
-                // Custome input validator base on different question type
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    switch (question.type) {
-                      case "sID":
-                        if (validatesID(value)) return Promise.resolve();
-                        return Promise.reject(new Error("Invalid sID field"));
+        {/* Pop up message when a form is submit */}
+        {contextHolder}
 
-                      case "Name":
-                        if (validatesName(value)) return Promise.resolve();
-                        return Promise.reject(new Error("Invalid name"));
-
-                      default:
-                        return Promise.resolve();
-                    }
-                  },
-                }),
-              ]}
-            >
-              {/* Rendering the multiple choice input directly due to the need of choice attribute from question */}
-              {question.type == "Multiple choice" ? (
-                <Radio.Group>
-                  {choices.map((choice) => {
-                    return <Radio value={choice}>{choice}</Radio>;
-                  })}
-                </Radio.Group>
-              ) : (
-                // If not multiple choice question, then using the typeMapping to map to the corresponding question type
-                typeMapping[question.type]
-              )}
-            </Form.Item>
-          );
-        })}
-
-        <Form.Item
-          wrapperCol={{
+        <Form
+          layout="vertical"
+          form={form}
+          name="manual input"
+          labelCol={{
+            span: 22,
             offset: 1,
           }}
+          wrapperCol={{
+            offset: 1,
+            span: 22,
+          }}
+          style={{
+            maxWidth: "100%",
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Space></Space>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          {/* Mapping each question into an input field using the question attributes */}
+          {questions.map((question) => {
+            let title = question.title.slice();
+            let choices = question.choice;
+            return (
+              <Form.Item
+                key={question.id}
+                label={title.charAt(0).toUpperCase() + title.slice(1)}
+                name={`${question.title}-${question.id}`} // Combining name with object id to create unique name for form input
+                rules={[
+                  {
+                    required: question.required,
+                    messsage: `Please input your ${title}`,
+
+                    type: question.type == "Date" ? "object" : "string",
+                  },
+                  // Custome input validator base on different question type
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      switch (question.type) {
+                        case "sID":
+                          if (validatesID(value)) return Promise.resolve();
+                          return Promise.reject(new Error("Invalid sID field"));
+
+                        case "Name":
+                          if (validatesName(value)) return Promise.resolve();
+                          return Promise.reject(new Error("Invalid name"));
+
+                        default:
+                          return Promise.resolve();
+                      }
+                    },
+                  }),
+                ]}
+              >
+                {/* Rendering the multiple choice input directly due to the need of choice attribute from question */}
+                {question.type == "Multiple choice" ? (
+                  <Radio.Group>
+                    {choices.map((choice) => {
+                      return <Radio value={choice}>{choice}</Radio>;
+                    })}
+                  </Radio.Group>
+                ) : (
+                  // If not multiple choice question, then using the typeMapping to map to the corresponding question type
+                  typeMapping[question.type]
+                )}
+              </Form.Item>
+            );
+          })}
+
+          <Form.Item
+            wrapperCol={{
+              offset: 1,
+            }}
+          >
+            <Space></Space>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+      {/* Manual Input Button */}
+      <Button ghost className=" mt-4 w-full" type="primary" onClick={showModal}>
+        <span className="text-black-100"> Manual Input </span>
+      </Button>
     </>
   );
 }
