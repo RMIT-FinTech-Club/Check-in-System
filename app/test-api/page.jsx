eda06e3f192188af697e3754ed1634373a916e53
Hankaji <hankaji135@gmail.com>
2023-09-06 13:32:17 +0700

    minor changes on excelActions

--- app/test-api/page.jsx
+++ app/test-api/page.jsx
@@ -1,11 +1,16 @@
 "use client"
 import { useState } from "react";
+import { Button } from "antd";
 import axios from "axios"
 
 
 export default function Page() {
 
     const [responseData, setResponseData] = useState();
+
+    const [cell, setCell] = useState();
+
+    const [data, setData] = useState();
 
     async function accessExcel() {
         try {
@@ -38,14 +43,51 @@ export default function Page() {
           }
     }
 
+    async function goToCell() {
+      try {
+          const response = await axios.post('/api/excel/go-to-cell', {
+            cell_position: cell
+          });
+          setResponseData(response.data)
+        } catch (error) {
+          console.error('Error fetching data:', error)
+        }
+    }
+
+    async function addData() {
+      try {
+          const response = await axios.post('/api/excel/add-data', {
+            data: data
+          });
+          setResponseData(response.data)
+        } catch (error) {
+          console.error('Error fetching data:', error)
+        }
+    } 
+
     return (
         <div className="content">
             <h1>This will test the api</h1>
             {responseData && <p>{JSON.stringify(responseData, null, 2)}</p>}
-            <div>
-                <button onClick={accessExcel}>access Excel</button>
-                <button onClick={testExcel}>test Excel</button>
-                <button onClick={dcExcel}>disconnect Excel</button>
+            <div className="mt-4 [&>*]:m-2">
+                <Button onClick={accessExcel}>access Excel</Button>
+                <Button onClick={testExcel}>test Excel</Button>
+                <Button onClick={dcExcel}>disconnect Excel</Button>
+
+                <div className="flex flex-col gap-3">
+                  <div className="flex gap-3 items-center">
+                    <Button onClick={goToCell}>Go to cell</Button>
+                    <input className="p-1 border border-gray-300 rounded-md"
+                     type="text" placeholder="input cell"
+                     onChange={e => setCell(e.target.value)} />
+                  </div>
+                  <div className="flex gap-3 items-center">
+                    <Button onClick={addData}>Add data</Button>
+                    <input className="p-1 border border-gray-300 rounded-md"
+                     type="text" placeholder="input data"
+                     onChange={e => setData(e.target.value)} />
+                  </div>
+                </div>
             </div>
         </div>
     )
