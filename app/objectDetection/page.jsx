a7e7806675c88bb4af5f5cd87ef8e860b15afb85
NguyenNguyen205 <92852929+NguyenNguyen205@users.noreply.github.com>
2023-10-15 17:14:49 +0700

    Testing socket io

--- app/objectDetection/page.jsx
+++ app/objectDetection/page.jsx
@@ -1,45 +1,23 @@
-"use client"
+"use client";
 import { WebcamFeed } from "../../components/ObjectDetection";
-import { TextDisplay } from "../../components/GetText";
-import { useState, useEffect } from 'react';
-// import io from 'socket.io-client';
-
-// const socket = io.connect('http://localhost:3000/objectDetection');
-
-// const YourComponent = () => {
-//     // State to store the signal data
-//     const [signal, setSignal] = useState('');
-  
-//     // Listen for the 'screenshot_saved' event
-//     useEffect(() => {
-//       socket.on('screenshot_saved', (data) => {
-//         // Handle the signal data here
-//         console.log('Signal received:', data);
-//         setSignal(data.message); // Update the state with the signal message
-//       });
-  
-//       return () => {
-//         // Clean up the event listener when the component unmounts
-//         socket.off('screenshot_saved');
-//       };
-//     }, []);
-  
-//     return (
-//       <div>
-//         <h1>Check for signal</h1>
-//         <p>Signal: {signal}</p>
-//         {/* Your other React components */}
-//       </div>
-//     );
-//   };
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 
 export default function Page() {
+  // initialize socket
+  const socket = io("http://localhost:5328");
+  const [screenshotStatus, setScreenshotStatus] = useState("Initializing...");
 
-    return (
-        <div className="content">
-            <h1>Camera Feed</h1>
-            <WebcamFeed />
-            {/* <YourComponent /> */}
-        </div>
-    )
+  // socket io
+  socket.on("message", (res) => {
+    console.log(res);
+    setScreenshotStatus("Receive an event");
+  });
+  return (
+    <div className="content">
+      <h1>Camera Feed</h1>
+      <WebcamFeed />
+      <p>ScreenshotStatus: {screenshotStatus}</p>
+    </div>
+  );
 }
