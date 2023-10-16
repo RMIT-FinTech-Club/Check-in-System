1e24e983e8eb25eed9060d71d889a546dfd98170
Cullen179 <itslamemail@gmail.com>
2023-10-16 23:31:14 +0700

    Receive signal

--- components/CameraCheckin.jsx
+++ components/CameraCheckin.jsx
@@ -3,10 +3,34 @@ import { CameraOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import ManualForm from "@/components/ManualForm";
 import { WebcamFeed } from "./ObjectDetection";
+import { io } from "socket.io-client";
+
 
 export default function CameraCheckin({ questions }) {
+  // Fetch scan data when received signal
+  function getScannedData() {
+    fetch("/api/objectDetection/get_text")
+      .then((response) => response.json())
+      .then((data) => {
+        setScannedData(data);
+      })
+      .catch((err) => console.log(err));
+  } 
+
+  function handleClose() {
+    setIsReceived(false);
+  }
+
+  const socket = io("http://localhost:5328");
+
+
   // Handling camera opening and closing
   const [cameraOpen, setCameraOpen] = useState(false);
+  const [isReceived, setIsReceived] = useState(false);
+  const [scannedData, setScannedData] = useState(null);
+
+  const [test, setTest] = useState("test");
+
   const showCamera = () => {
     setCameraOpen(true);
   };
@@ -15,15 +39,9 @@ export default function CameraCheckin({ questions }) {
     setCameraOpen(false);
   };
 
-  // Handling open manual input form
-  const [open, setOpen] = useState(false);
-  const showModal = () => {
-    setOpen(true);
-  };
-
-  const handleCancel = () => {
-    setOpen(false);
-  };
+  socket.on("message", (res) => {
+    setIsReceived(true);
+  });
 
   return (
     <>
@@ -47,7 +65,11 @@ export default function CameraCheckin({ questions }) {
           cancelButtonProps={{ style: { display: "none" } }}
         >
           <WebcamFeed />
-          <ManualForm questions={questions}></ManualForm>
+          {
+            isReceived &&
+              <ManualForm scannedData={scannedData} questions={questions} isOpen={isReceived} cancelFunc={handleClose}></ManualForm>
+          }
+         
         </Modal>
         <div className="flex justify-center">
           <Button
