2e1210f7ca63b0f85e48bbb67350546086b99b9d
Hankaji <hankaji135@gmail.com>
2024-03-10 20:14:08 +0700

    Migrated from Redis to MongoDB

--- api/controller/db/excel_data.py
+++ api/controller/db/excel_data.py
@@ -2,134 +2,108 @@ import redis
 import json
 import uuid
 from flask import Blueprint, request, jsonify
+from pymongo.mongo_client import MongoClient
+from urllib.parse import quote_plus, quote
+from bson import ObjectId
 
 excel_data_bp = Blueprint("excel_data", __name__)
-
-# r = redis.StrictRedis(
-#     host="redis-13371.c1.ap-southeast-1-1.ec2.cloud.redislabs.com",
-#     port=13371,
-#     password="egfwA1Xhkr9S736RuT3hmbUv4CQAk1ER",
-# )
 
 r = redis.Redis(
     host="localhost",
     port=6379,
 )
 
+username = quote_plus("fintech")
+password = quote_plus("poiuytrewq")
+
+uri = f"mongodb+srv://{username}:{password}@checker-cluster.ptsb8e0.mongodb.net/?retryWrites=true&w=majority&appName=Checker-Cluster"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(f"An error occurred while connecting to the server: {e}")
+
+db = client["checker"]
+excel_data_col = db["excel_data"]
+# id = excel_data_col.find_one({'url': "https://rmiteduau.sharepoint.com/:x:/r/sites/RMITFinTechClub2023/Shared%20Documents/2023%20FinTech%20Club%20Master%20Folder/Sem%20B/Departments/Technology/Computer%20Vision%20Project/Book.xlsx?d=wc133eaeca703446686947dd77f977172&csf=1&web=1&e=Tl81AC"})
+# print(id)
+
 @excel_data_bp.route('/')
 def index():
     return "Excel database api index page"
 
-# @excel_data_bp.route('/', methods=['GET'])
-# def get_excel_data():
-#     dummy_data = {
-#         "thisDataIsDummy": {
-#             "url": "https://www.google.com",
-#             "questions": [
-#                 {
-#                     "id": 0,
-#                     "title": "Your name (from dummy data)",
-#                     "type": "Name"
-#                 },
-#                 {
-#                     "id": 1,
-#                     "title": "Your sID (from dummy data)",
-#                     "type": "sID"
-#                 },
-#             ]
-#         }
-#     }
-    
-#     id = request.args.get('id')
-#     url = request.args.get('url')
-
-#     # If no id or url is provided, return error code
-#     if id is None or url is None:
-#         return jsonify({'message': 'no id or url provided'}), 400
-    
-    
-
-#     # Get data, if not found return error code
-#     response = dummy_data.get(id, None)
-#     if response is None:
-#         return jsonify({'message': 'no data found'}), 404
-
-#     return jsonify(response)
-
-
-@excel_data_bp.route('/r', methods=['POST'])
+@excel_data_bp.route('/db', methods=['POST'])
 def set_question():
     # return "Why are setting questions?"
 
-    question = request.get_json()
-    if question is None:
+    data = request.get_json()
+    if data is None:
         return jsonify({'message': 'no data provided'}), 400
-    if question.get('url') is None:
+    if data.get('url') is None:
         return jsonify({'message': 'no url provided'}), 400
     
-    # Store excel item in Redis
-    question_json = json.dumps(question)
-    id = str(uuid.uuid4())
-
-    r.set(id, question_json)
-
-    # Store id to url
-    r.set(question['url'], id)
+    # Store excel item in Database
+    excel_data_col.insert_one({
+        'url': data.get('url'),
+        'questions': data.get('questions')
+    })
 
     # print("Question set successfully in Redis.")
-    return jsonify({'message': 'Question set successfully in Redis.'}), 200
+    return jsonify({'message': 'Question set successfully in Database.'}), 200
 
 
-@excel_data_bp.route('/r/<id>', methods=['PUT'])
+@excel_data_bp.route('/db/<id>', methods=['PUT'])
 def update_question(id: str):
-    exist_data = r.get(id)
+    exist_data = excel_data_col.find_one({'_id': ObjectId(id)})
     questions = request.get_json().get('questions', None)
 
     if exist_data is None:
         return jsonify({'message': 'no data exist'}), 400
-    
-    # url = json.loads(exist_data).get('url')
 
     if questions is None:
         return jsonify({'message': 'no data provided'}), 400
 
-    # Store excel item in Redis
-    new_data = json.loads(exist_data)
-    new_data['questions'] = questions
-
-    question_json = json.dumps(new_data)
-    r.set(id, question_json)
+    # Store excel item in MongoDB
+    excel_data_col.update_one({'_id': ObjectId(id)}, {'$set': {'questions': questions}})
 
     # print("Question set successfully in Redis.")
     return jsonify({'message': 'Question updated successfully in Redis.'}), 200
 
 
-@excel_data_bp.route('/r/id', methods=['GET'])
+@excel_data_bp.route('/db/id', methods=['GET'])
 def get_id():
     url = request.args.get('url')
-    
+    url = quote(url, safe=':/?=&')
+
     if url is None:
         return jsonify({'message': 'no url provided'}), 400
-    
-    id = r.get(url)
+
+    id = excel_data_col.find_one({'url': url})
+    print(id)
     if id:
-        return jsonify({'message': 'Id retrived', 'id': id.decode() }), 200
+        return jsonify({'message': 'Id retrived', 'id': str(id.get('_id')) }), 200
     else:
         return jsonify({'message': 'No id found'}), 404
 
 
-@excel_data_bp.route('/r/<id>', methods=['GET'])
+@excel_data_bp.route('/db/<id>', methods=['GET'])
 def get_question(id: str):
     # return "Why am i here?"
     if id is None:
         return jsonify({'message': 'no id provided'}), 400
 
-    question_json = r.get(id)
+    question_json = excel_data_col.find_one({'_id': ObjectId(id)})
+    print(question_json)
 
     if question_json:
-        question = json.loads(question_json)
-        print("Question retrieved successfully from Redis.")
-        return question, 200
+        questions = question_json.get('questions')
+        print("Question retrieved successfully from MongoDB.")
+        return jsonify({'questions': questions}), 200
     else:
         print("No question found in Redis.")
         return jsonify({'message': 'no data found'}), 404
@@ -151,12 +125,4 @@ if __name__ == "__main__":
     #         ]
     #     }
     # question_json = json.dumps(question)
-    # id = uuid.uuid4()
-
-    # # r.set(id, question_json)
-    # r.set("WHAT", "WHY?")
-    # print("Question set successfully in Redis.")
-    # id = r.get("https://www.google.com")
-    all = r.keys()
-    # id = uuid.uuid4()
-    print(all)
+    pass
